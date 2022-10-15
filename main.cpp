d9360e2f7cd98d512d836ad850b80d491575ac33
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2022-10-15 12:04:38 -0500

    End project

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+/*This program was developed to register users with email and a password. 
+In order to store this information, a record was created in a text file with the email of each registered user. 
+For the user, a menu is displayed where they choose whether to register or log in.*/
 
 #include <iostream>
 #include <fstream>
@@ -5,14 +8,20 @@
 
 using namespace std;
 
+// Global variables
+string strMail;
+string strPassword;
+
 ///<summary>
 /// Method to validate if the user is logged in
 ///</summary>
 bool BlnLogin()
 {
     /// Variables
-    string strPassword, strMail, strUser, strPaw;
+    string strUser;
+    string strPaw;
 
+    //Enter main information to login 
     cout<<"Enter a mail: "; cin >> strMail;
     cout<<"Enter a password: "; cin >> strPassword;
 
@@ -20,9 +29,10 @@ bool BlnLogin()
     ifstream read("C:\\src\\" + strMail + ".txt");
     getline(read, strUser);
     getline(read, strPaw);
+    
 
     // Validate mail and password
-    if(strUser == strMail && strPaw == strPassword)
+    if(strUser == "Mail: " + strMail && strPaw == "Password: " + strPassword)
     {
         return true;
     }
@@ -31,40 +41,81 @@ bool BlnLogin()
         return false;
     }
 }
-
+///<summary>
+/// Main method
+///</summary>
 int main()
 {
     // Variables
-    int choice;
-    string strPassword, strMail;
+    int intChoice;
+    string strName, strLastname, strPhone;
     bool blnStatus;
     
+    system("CLS"); // Clear screen
     //Menu
-    cout <<"1: Register\n2: Login\nYour choice: "; cin >> choice;
-    if(choice == 1)
+    cout << "Menu";
+    cout <<"\n===========================\n";
+    cout << "1: Register\n";
+    cout << "2: Login\n";
+    cout << "===========================\n";
+    cout << "Enter your choice: "; cin >> intChoice;
+   
+    system("CLS");// Clear screen
+    if(intChoice == 1)
     {
-      cout << "Enter a mail: "; cin >> strMail;
+      //Register
+      cout << "Please, enter all information required\n";
+      cout << "Enter your mail: "; cin >> strMail;
       cout << "Enter a password: "; cin >> strPassword;
-      
+      cout <<"Enter your name: "; cin >> strName;
+      cout <<"Enter your lastname: "; cin >> strLastname;
+      cout << "Enter your phone number: "; cin >> strPhone;
+           
       //Save file
       ofstream file;
+
       file.open("C:\\src\\" + strMail + ".txt");
-      file << strMail << endl << strPassword << endl;
+      file << "Mail: " + strMail << endl;
+      file << "Password: " + strPassword << endl;
+      file <<  "Name: " + strName << endl;
+      file << "Lastname: " + strLastname << endl; 
+      file << "Phone: " + strPhone << endl;
+    
       file.close();
       main();
     }
-    else if(choice == 2)
+    else if(intChoice == 2)
     {
+        // Login
+        cout << "Please, enter all information required\n";
         blnStatus = BlnLogin();
         if(!blnStatus)
         {
+            // Login failed
             cout << "Login is not right!" << endl;
             system("PAUSE");
             return 0;
         }
         else
         {
-            cout << "Login succesfully" << endl;
+            // Login successfully
+            cout << "Login successfully" << endl;
+            ifstream read("C:\\src\\" + strMail + ".txt");
+            getline(read, strMail);
+            getline(read, strPassword);
+            getline(read, strName);
+            getline(read, strLastname);
+            getline(read, strPhone);
+
+            system("CLS");// Clear screen
+            
+            cout <<"User information: \n";
+            cout << "Mail: " + strMail << endl;
+            cout << "Password: " + strPassword  << endl;
+            cout << "Name: " + strName  << endl;
+            cout << "Lastname: " + strLastname  << endl;
+            cout << "Phone: " + strPhone  << endl;
+
             system("PAUSE");
             return 1;
         }
